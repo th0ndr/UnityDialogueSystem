ab057308c4820baffb6eee1de7d96879370f021a
Ricardo Bustos González <ricardobustosglz@gmail.com>
2018-01-01 18:41:24 -0600

    Added WaitTime
    WaitTime Parser, in each dialogue.

--- Assets/Example/Scripts/Character.cs
+++ Assets/Example/Scripts/Character.cs
@@ -8,5 +8,6 @@ public class Character : MonoBehaviour
 	public string name;
 	public Sprite standardExpression;
 	public Expression[] expressions;
+	public AudioClip voice;
 
 }
--- Assets/Example/Scripts/DialogueManager.cs
+++ Assets/Example/Scripts/DialogueManager.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
-
+	[HideInInspector]
 	public Text dialogueText;
+
+	[HideInInspector]
 	public Image imageText;
 
+	[HideInInspector]
 	public Animator animator;
+
+	[HideInInspector]
+	public float waitTime = .01f;
+
+	public float voiceVolume = 1f;
+
 
 	private Queue<string> sentences;
 	private Queue<Sprite> sprites;
+	private Queue<AudioClip> voices;
+	private AudioSource source;
+	private bool betweenChars = true;
 
 
 	void Start () 
 	{
 		sentences = new Queue<string>();
 		sprites = new Queue<Sprite> ();
+		voices = new Queue<AudioClip> ();
 		imageText = imageText.GetComponent<Image>();
+		source = GetComponent<AudioSource>();
 	}
 
 	void Update ()
@@ -33,8 +47,8 @@ public class DialogueManager : MonoBehaviour {
 	{
 		animator.SetBool("IsOpen", true);
 
-
-
+		voices.Clear ();
+		sprites.Clear ();
 		sentences.Clear();
 
 		foreach(Sentence sentence in dialogue.sentences)
@@ -45,6 +59,7 @@ public class DialogueManager : MonoBehaviour {
 			{
 				sprites.Enqueue (sentence.character.standardExpression);
 				sentences.Enqueue(paragraph);
+				voices.Enqueue (sentence.character.voice);
 			}
 
 		}
@@ -62,21 +77,51 @@ public class DialogueManager : MonoBehaviour {
 
 		imageText.sprite = sprites.Dequeue ();
 		string sentence = sentences.Dequeue();
-	
+		AudioClip audio = voices.Dequeue ();
+
 		StopAllCoroutines();
 
-		StartCoroutine(TypeSentence(sentence));
+		StartCoroutine(TypeSentence(sentence, audio));
 	}
 
-	IEnumerator TypeSentence(string sentence)
+	IEnumerator TypeSentence(string sentence, AudioClip audio)
 	{
+		string time = "";
+		bool parsing = false;
 		dialogueText.text = "";
 		foreach(char letter in sentence.ToCharArray())
 		{
-			dialogueText.text += letter;
-			yield return null;
+
+			if (letter == '[') 
+			{
+				parsing = true;
+			}
+
+			if (parsing) 
+			{
+				parsing = true;
+				if (letter == ']') {
+					parsing = false;
+					waitTime = float.Parse (time) * .001f;
+					time = "";
+				} 
+
+				if (letter != '[' && letter != ']') {
+					time += letter;
+				}
+
+			}
+			else
+			{
+				dialogueText.text += letter;
+				source.PlayOneShot (audio, voiceVolume);
+				yield return new WaitForSeconds(waitTime);
+			}
+
 		}
 	}
+
+
 
 	void EndDialogue()
 	{
