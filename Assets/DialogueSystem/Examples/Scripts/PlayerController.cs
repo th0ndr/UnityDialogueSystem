b81d59741a63d353c9f055ffb38ab6e26edf4282
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-06-20 19:41:54 -0500

    Remove unnesesary classes and elements
    Dialogue: variableName and variableValue
    Dialogue Trigger Class
    Variables Manager Class
    General Trigger Class

--- Assets/DialogueSystem/Examples/Scripts/PlayerController.cs
+++ Assets/DialogueSystem/Examples/Scripts/PlayerController.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
-    public DialogueTrigger dialogue;
+    
 	
 	void Update () 
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            dialogue.TriggerDialogue();
-        }
 
 	}
 
